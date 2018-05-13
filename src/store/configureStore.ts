4eda2c67e17a2bf4e286d917d28c718d9ab3a5c9
Mickey Loreti <mloreti10@gmail.com>
2018-05-12 20:23:19 -0700

    Setup jest configuration

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -3,7 +3,7 @@ import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import thunkMiddleware from 'redux-thunk';
 
-import rootReducer from 'state/reducers';
+import rootReducer from '../state/reducers';
 
 const preloadedState = {};
 
