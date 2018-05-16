e33ef5901b46277051a44f2efdbe81779ed5ac2a
Mickey Loreti <mloreti10@gmail.com>
2018-05-15 19:20:17 -0700

    Add tachyons components and create homepage

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import Home from '../Home';
+
 const Routes = () => (
   <Switch>
-    <Route exact path="/" render={() => <h1>hi</h1>} />
-    <Route render={() => <h1>Nothing to see here</h1>} />
+    <Route exact path="/" component={Home} />
   </Switch>
 );
 
