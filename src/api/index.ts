0b28007952e14790f09045b207338a5c35ccf450
Mickey Loreti <mloreti10@gmail.com>
2018-06-17 21:41:36 -0700

    Add functions for getting data

--- src/api/index.ts
+++ src/api/index.ts
@@ -18,14 +18,32 @@ export const updateData: FirebaseWrite = (path, data) => {
   });
 };
 
+export type GetData = <T>(path: string) => Promise<T>;
+
+export const getData: GetData = <T>(path) => {
+  return new Promise(res => {
+    firebase.database().ref(path).once('value').then(snapshot => {
+      res(snapshot.val());
+    });
+  });
+};
+
 export const currentUser = () => {
   return firebase.auth().currentUser;
 };
 
 export const setBoot = (bootInfo: BootInfo) => {
-  writeData('/boots', bootInfo);
+  return writeData('/boots', bootInfo);
 };
 
 export const updateBoot = (id: number, data: Partial<BootInfo>) => {
-  updateData(`/boots/${id}`, data);
+  return updateData(`/boots/${id}`, data);
 };
+
+export type GetAllBoots = () => Promise<BootInfo[]>;
+
+export const getAllBoots: GetAllBoots = () => getData('boots');
+
+export type GetBoot = (id: string) => Promise<BootInfo>;
+
+export const getBoot: GetBoot = id => getData(`boots/${id}`);
