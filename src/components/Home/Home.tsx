a9cd0e7c1e0a265458ed31beb59b77608634540c
Mickey Loreti <mloreti10@gmail.com>
2018-07-01 14:02:38 -0400

    Remove spinner from home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import Header from '../Header';
 import { Container, Heading } from '../Styled';
-import Spinner from '../Styled/Spinner';
 
 const Home = () => (
   <Container>
     <Header/>
     <Heading>Hi</Heading>
-    <Spinner />
   </Container>
 );
 
