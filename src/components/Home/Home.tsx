e55c463e5f6113edd2407f2a0684178aada228ab
Mickey Loreti <mloreti10@gmail.com>
2018-07-01 14:01:33 -0400

    Create loading spinner component

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../Header';
 import { Container, Heading } from '../Styled';
+import Spinner from '../Styled/Spinner';
 
 const Home = () => (
   <Container>
     <Header/>
     <Heading>Hi</Heading>
+    <Spinner />
   </Container>
 );
 
