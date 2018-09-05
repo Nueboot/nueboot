13a59c016a13aeaa2f56b35d0f473c1297a00328
Mickey Loreti <mloreti10@gmail.com>
2018-09-04 21:34:04 -0700

    Move bootstrap to own dir

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,20 +3,16 @@ import { Provider } from 'react-redux';
 import { ConnectedRouter } from 'react-router-redux';
 
 import 'tachyons';
-import BootstrapApp, { history, store } from '../Bootstrap';
+import BootstrapApp, { history, store } from '../../bootstrap';
 import Routes from '../Routes';
 import './App.css';
 
 BootstrapApp();
 
-export default class App extends Component {
-  public render() {
-    return (
-      <Provider store={store}>
-        <ConnectedRouter history={history}>
-          <Routes />
-        </ConnectedRouter>
-      </Provider>
-    );
-  }
-}
+export default () => (
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Routes />
+    </ConnectedRouter>
+  </Provider>
+);
