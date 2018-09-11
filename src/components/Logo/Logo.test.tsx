6ff8636b8d59df9ee9c91beade6bf8e7b18e69f3
Mickey Loreti <mloreti10@gmail.com>
2018-09-10 20:46:45 -0700

    Add Logo component

---
+++ src/components/Logo/Logo.test.tsx
@@ -0,0 +1,44 @@
+import { shallow, ShallowWrapper } from 'enzyme';
+import React from 'react';
+import Logo, { LogoProps } from './Logo';
+
+describe('Logo', () => {
+  let component: ShallowWrapper<LogoProps>;
+  let props: LogoProps;
+
+  describe('without a color prop', () => {
+    beforeEach(() => {
+      component = shallow(<Logo />);
+    });
+
+    it('renders correctly', () => {
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  describe('with a color prop', () => {
+    beforeEach(() => {
+      props = {
+        color: 'red',
+      };
+      component = shallow(<Logo {...props} />);
+    });
+
+    it('renders correctly', () => {
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  describe('with a class', () => {
+    beforeEach(() => {
+      props = {
+        className: 'logo',
+      };
+      component = shallow(<Logo {...props} />);
+    });
+
+    it('renders correctly', () => {
+      expect(component).toMatchSnapshot();
+    });
+  });
+});
