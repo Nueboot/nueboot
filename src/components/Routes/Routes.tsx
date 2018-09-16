3fd2ff7531850eaf3cf5728de25876e1fec1f32a
Mickey Loreti <mloreti@users.noreply.github.com>
2018-09-15 23:57:18 -0700

    Refactor routes (#37)

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import { Redirect, Route, Switch } from 'react-router-dom';
 
-import AuthModal from '../AuthModal';
 import Boot from '../Boot';
 import Boots from '../Boots';
-import Header from '../Header';
 import Home from '../Home';
 
 const Routes = () => (
-  <div className="app flex flex-column">
-    <Header />
-    <div className="content">
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/boots" component={Boots} />
-        <Route exact path="/boots/:bootId" component={Boot} />
-        <Redirect to="/" />
-      </Switch>
-    </div>
-    <AuthModal />
-  </div>
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/boots" component={Boots} />
+    <Route exact path="/boots/:bootId" component={Boot} />
+    <Redirect to="/" />
+  </Switch>
 );
 
 export default Routes;
