b3091051f34bd1600c82ce04d48e222b159521ca
Mickey Loreti <mloreti10@gmail.com>
2018-09-18 22:02:23 -0700

    Decrease Text font-size

--- src/components/Styled/Text.tsx
+++ src/components/Styled/Text.tsx
@@ -3,8 +3,8 @@ import styled from 'styled-components';
 const Text = styled.span`
   font-family: 'Heebo', -apple-system, BlinkMacSystemFont, 'Helvetica',
     sans-serif;
-  font-size: 1rem;
-  color: rgb(0, 0, 0, 0.7);
+  font-size: 0.875rem;
+  color: rgba(0, 0, 0, 0.7);
 `;
 
 export default Text;
