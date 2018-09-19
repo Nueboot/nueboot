a28fd98046d5e037971ff5cad14fed2281b6172b
Mickey Loreti <mloreti10@gmail.com>
2018-09-18 22:03:35 -0700

    Delete Input component

--- src/components/Styled/index.ts
+++ src/components/Styled/index.ts
@@ -4,7 +4,6 @@ export { default as Logo } from './Logo';
 export { default as Text } from './Text';
 export { default as Heading } from './Heading';
 export { default as Subheading } from './Subheading';
-export { default as Input } from './Input';
 export { default as Spinner } from './Spinner';
 export { default as Image } from './Image';
 export { default as NavigationLink } from './NavigationLink';
