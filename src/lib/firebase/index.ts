ed0f13bc21f394099c00d53815cd700608a4b14a
Mickey Loreti <mloreti10@gmail.com>
2018-10-15 21:26:49 -0700

    Update database url and authdomain in firebase config

--- src/lib/firebase/index.ts
+++ src/lib/firebase/index.ts
@@ -2,11 +2,11 @@ import firebase from 'firebase';
 
 const config = {
   apiKey: 'AIzaSyDWHl06j32I3-4n9igIKGpcKdaqOb7PwTo',
-  authDomain: 'nueboot-app.firebaseapp.com',
-  databaseURL: 'https://nueboot.com',
+  authDomain: 'nueboot.com',
+  databaseURL: 'https://nueboot-app.firebaseio.com',
+  messagingSenderId: '23567420237',
   projectId: 'nueboot-app',
   storageBucket: 'nueboot-app.appspot.com',
-  messagingSenderId: '23567420237',
 };
 
 export default firebase.initializeApp(config);
