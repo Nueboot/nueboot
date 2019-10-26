3072fdb8af4868fc4c48ca8ebc928cc5e334c32a
Mickey Loreti <mloreti10@gmail.com>
2019-10-26 11:17:51 -0700

    Update dependencies

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-import App from 'components/App';
 import 'typeface-heebo';
+
+import App from './components/App';
 
 const root = document.querySelector('#root');
 
