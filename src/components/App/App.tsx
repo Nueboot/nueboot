dba06bd6195040393ff44d372054df7cad2f572b
Mickey Loreti <mloreti10@gmail.com>
2020-01-21 12:18:22 -0800

    Move header to router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,12 @@
 import React, { Suspense } from "react";
 
 import Router from "../Router";
-import Header from "../Header";
 import GlobalStyles from "./Global.styles";
 
 export default function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <GlobalStyles />
-      <Header />
       <Router />
     </Suspense>
   );
--- src/components/Router/Router.tsx
+++ src/components/Router/Router.tsx
@@ -1,12 +1,18 @@
 import * as React from "react";
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+
 import BootsList from "../BootsList";
+import Boot from "../Boot";
+import Header from "../Header";
 
 function Router() {
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         <Route exact path="/boots" component={BootsList} />
+        <Route exact path="/boots/:bootId" component={Boot} />
+        <Redirect to="/" />
       </Switch>
     </BrowserRouter>
   );
