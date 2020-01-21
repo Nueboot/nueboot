57bc799c77d8fd109d7a294e7b456c11f483f73d
Mickey Loreti <mloreti10@gmail.com>
2020-01-21 12:17:17 -0800

    Add public path to output

--- webpack.config.js
+++ webpack.config.js
@@ -13,8 +13,9 @@ module.exports = {
   entry: "./src/index.tsx",
 
   output: {
-    path: path.join(__dirname, "/dist"),
-    filename: "bundle.min.js"
+    path: path.join(__dirname, "dist"),
+    filename: "bundle.min.js",
+    publicPath: "/"
   },
 
   resolve: {
