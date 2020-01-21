b3bb0722d17a23a964f4b20d876f267fe59206d4
Mickey Loreti <mloreti10@gmail.com>
2020-01-21 09:36:13 -0800

    Add header to App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,14 @@
 import React, { Suspense } from "react";
 
 import Router from "../Router";
+import Header from "../Header";
 import GlobalStyles from "./Global.styles";
 
 export default function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <GlobalStyles />
+      <Header />
       <Router />
     </Suspense>
   );
