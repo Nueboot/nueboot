a7d29be0f4c0c30df67ee34f35dad1c6369583ac
Mickey Loreti <mloreti10@gmail.com>
2020-01-21 09:45:06 -0800

    Update global styles with anchor style

--- src/components/App/Global.styles.tsx
+++ src/components/App/Global.styles.tsx
@@ -5,6 +5,16 @@ const GlobalStyle = createGlobalStyle`
   body {
     font-family: 'Heebo';
   }
+
+  a {
+    color: black;
+    text-decoration: none;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+
 `;
 
 export default GlobalStyle;
