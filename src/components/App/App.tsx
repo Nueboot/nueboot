09ee4f99197816554b38595368acc37115a7abf9
Mickey Loreti <mloreti10@gmail.com>
2020-01-20 21:35:55 -0800

    Add global styles with font

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,12 @@
 import React, { Suspense } from "react";
-import "typeface-heebo";
 
 import Router from "../Router";
+import GlobalStyles from "./Global.styles";
 
 export default function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
+      <GlobalStyles />
       <Router />
     </Suspense>
   );
---
+++ src/components/App/Global.styles.tsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+import "typeface-heebo";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Heebo';
+  }
+`;
+
+export default GlobalStyle;
