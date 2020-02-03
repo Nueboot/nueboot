c7749548167c035b5f78f6ebe033ebf0503f4f46
Mickey Loreti <mloreti10@gmail.com>
2020-02-03 15:59:57 -0800

    Update App styling

---
+++ src/components/App/App.styles.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  margin-top: 60px;
+
+  @media (max-width: 960px) {
+    margin: 60px 20px;
+  }
+`;
