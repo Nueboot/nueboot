73d6dcd9ef851112a73777e6e898bd9e5800f674
Mickey Loreti <mloreti10@gmail.com>
2020-02-03 15:58:23 -0800

    Turn off prop types for typescript files

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,6 +6,14 @@ module.exports = {
     "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
+  overrides: [
+    {
+      files: ["**/*.tsx"],
+      rules: {
+        "react/prop-types": "off"
+      }
+    }
+  ],
   plugins: ["import"],
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
