3cc98d3880a4eb9ef78950f6454d59fc14c45971
Mickey Loreti <mloreti10@gmail.com>
2020-02-03 16:02:46 -0800

    Create constants of brands and models

---
+++ src/data/constants.ts
@@ -0,0 +1,18 @@
+import { Brands } from "../types/boots";
+
+export const BRANDS: Brands[] = [
+  "Adidas",
+  "Nike",
+  "New Balance",
+  "Puma",
+  "Under Armour"
+];
+
+export const MODELS = [
+  "Mercurial Vapor",
+  "Predator",
+  "X 18.1",
+  "Ace 17.1",
+  "Future 18.1",
+  "Hypervenom Phantom"
+];
