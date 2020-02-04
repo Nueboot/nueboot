ec4880fff41fbdcf01e2cc383e760836362fc3be
Mickey Loreti <mloreti10@gmail.com>
2020-02-03 16:00:08 -0800

    Add container and footer to App

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,13 +1,18 @@
 import React, { Suspense } from "react";
 
 import Router from "../Router";
+import Footer from "../Footer";
 import GlobalStyles from "./Global.styles";
+import { Container } from "./App.styles";
 
 export default function App() {
   return (
     <Suspense fallback={<div>Loading...</div>}>
       <GlobalStyles />
-      <Router />
+      <Container>
+        <Router />
+      </Container>
+      <Footer />
     </Suspense>
   );
 }
